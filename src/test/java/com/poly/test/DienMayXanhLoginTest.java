e7e75dd134ab21777069f3d4ab3b9bd4bb556ec5
ASUS <thqhnhah06@gmail.com>
2026-03-03 12:05:17 +0700

    ci/cd

--- src/test/java/com/poly/test/DienMayXanhLoginTest.java
+++ src/test/java/com/poly/test/DienMayXanhLoginTest.java
@@ -29,7 +29,7 @@ public class DienMayXanhLoginTest {
         options.addArguments("--incognito"); 
         options.addArguments("--disable-notifications"); 
         options.addArguments("--disable-popup-blocking"); 
-        options.addArguments("--headless=new");
+        //options.addArguments("--headless=new");
         options.setExperimentalOption("excludeSwitches", new String[]{"enable-automation"});
 
         driver = new ChromeDriver(options);
